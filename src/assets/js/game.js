c4de7d75d8dcd618be7eea1aecc5013ff2e33fa9
p-herbert <pete.a.herbert@gmail.com>
2017-02-08 00:25:00 -0800

    (feat) Create game class

---
+++ src/assets/js/game.js
@@ -0,0 +1,32 @@
+class game {
+  constructor(n) {
+    this._size = n;
+    this._cells = [];
+  }
+
+  cells() {
+  }
+
+  get(row, col) {
+  }
+
+  neighbors(row, col) {
+  }
+
+  tick() {
+  }
+
+  survives(row, col) {
+  }
+
+  dies(row, col) {
+  }
+
+  born(row, col) {
+  }
+
+  each(cb) {
+  }
+}
+
+module.exports = game;
