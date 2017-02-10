898ab74e38b10b4a2e90d7cf43fffe48cd43fe41
p-herbert <pete.a.herbert@gmail.com>
2017-02-09 22:26:32 -0800

    (feat) Implement Cell component

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
-const Cell = props => <div>Test String</div>;
+const Cell = props =>
+  <rect
+    className={props.status}
+    x={props.x}
+    y={props.y}
+    height={props.height}
+    width={props.width}
+  />;
 
 Cell.propTypes = {
-
-};
+  status: React.PropTypes.string.isRequired,
+  x: React.PropTypes.number.isRequired,
+  y: React.PropTypes.number.isRequired,
+  height: React.PropTypes.number.isRequired,
+  width: React.PropTypes.number.isRequired };
 
 export default Cell;
 
