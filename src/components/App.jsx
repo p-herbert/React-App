4a4405259c672e81ce6d9e53d5377c3b0ac112a8
p-herbert <pete.a.herbert@gmail.com>
2017-02-09 22:36:28 -0800

    (feat) Implement App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,17 +7,61 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      height: (100 / this.props.size),
+      width: (100 / this.props.size),
+      life: new Life(this.props.size),
+      cells: [] };
+
+    this.state.life.each((row, col) => {
+      this.state.cells.push(this.mapper(row, col));
+    });
+  }
+
+  componentDidMount() {
+    let next;
+
+    setInterval(() => {
+      next = [];
+      this.state.life.tick();
+
+      this.state.life.each((row, col) => next.push(this.mapper(row, col)));
+
+      this.setState({
+        cells: next });
+    }, 1000);
+  }
+
+  mapper(row, col) {
+    return {
+      x: row * this.state.height,
+      y: col * this.state.width,
+      key: this.state.life.index(row, col),
+      status: this.state.life.get(row, col) === 1 ? 'alive' : 'dead' };
   }
 
   render() {
     return (
       <svg className="board" viewBox="0 0 100 100" >
-        <Cell />
+        {
+          this.state.cells.map(cell =>
+            <Cell
+              status={cell.status}
+              x={cell.x}
+              y={cell.y}
+              height={this.state.height}
+              width={this.state.width}
+              key={cell.key}
+            />
+          )
+        }
       </svg>
     );
   }
 }
+
+App.propTypes = {
+  size: React.PropTypes.number.isRequired };
 
 export default App;
 
