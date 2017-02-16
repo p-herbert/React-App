c342c5d7f2a9495930ee14d27e362a9f7bdcc1b7
p-herbert <pete.a.herbert@gmail.com>
2017-02-16 13:08:14 -0800

    (feat) Add '/greetings' router

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const router = require('db/routers/greetRouter');
 
 // Create the Express application
 const app = express();
@@ -7,6 +8,9 @@ const app = express();
 // Add body-parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Add router(s)
+app.use(router);
 
 // Static assessts
 app.use(express.static('client'));
