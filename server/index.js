a24a62f6502b83c9386a2b7a5410946d1d6be908
p-herbert <pete.a.herbert@gmail.com>
2017-02-16 14:26:12 -0800

    (feat) Insert data

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 const app = require('./server');
 const db = require('db');
+const data = require('db/data/greetings');
 const Promise = require('bluebird');
 
 const port = 8080;
 
 db.sequelize.sync({ force: true })
-  .then(() => console.log('Database starting...'))
-  .then(() => app.listen(port, () => console.log('Server starting...')))
+  .then(() => {
+    console.log('Database starting...');
+
+    db.Greet
+      .bulkCreate(data)
+      .then(() => console.log('SUCCESS: Data Inserted'))
+      .then(() => app.listen(port, () => console.log('Server starting...')))
+      .catch(err => console.log(err));
+  })
   .catch(err => console.log(err));
 
