30be64cf37ee7b2dc715d11d5e955676504c2139
p-herbert <pete.a.herbert@gmail.com>
2017-02-16 14:22:36 -0800

    (feat) Connect to database and start server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const app = require('./server');
+const db = require('db');
+const Promise = require('bluebird');
 
 const port = 8080;
 
-app.listen(port, () => console.log('Server starting...'));
+db.sequelize.sync({ force: true })
+  .then(() => console.log('Database starting...'))
+  .then(() => app.listen(port, () => console.log('Server starting...')))
+  .catch(err => console.log(err));
 
