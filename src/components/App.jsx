f2d5cfdb1706915fe84ac9923eafed862517373f
p-herbert <pete.a.herbert@gmail.com>
2017-02-16 23:18:47 -0800

    (feat) Add get method and greetings state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import * as _ from 'lodash';
+import 'whatwg-fetch';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      greetings: [] };
+
+    this.get = this.get.bind(this);
+  }
+
+  get(endpoint, cb) {
+    fetch(endpoint)
+      .then(res => res.json())
+      .then(data => cb(null, data))
+      .catch(err => cb(err, null));
+  }
+
+  componentDidMount() {
+    this.get('/greetings', (err, data) => this.setState({ greetings: data }));
   }
 
   render() {
