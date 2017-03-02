e42921939f1c6e0c36a2f1713aaf92ce6884099d
p-herbert <pete.a.herbert@gmail.com>
2017-03-02 02:16:26 -0800

    (feat) Create helper methods

---
+++ src/assets/js/helpers.js
@@ -0,0 +1,13 @@
+function isPalindrome(str) {
+  str = str.toLowerCase();
+  return str === str.split('').reverse().join('');
+}
+
+function paragraphs(text) {
+  return text.replace('/\r\n/g', '\n').split('\n');
+}
+
+module.exports = {
+  isPalindrome: isPalindrome,
+  paragraphs: paragraphs };
+
