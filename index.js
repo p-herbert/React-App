85b1b5c5b7c4331ed376b5e0cefe493e9ffc1c9a
p-herbert <pete.a.herbert@gmail.com>
2017-03-02 00:01:29 -0800

    (refactor) Remove database

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
 const app = require('server');
-const db = require('db');
-const data = require('db/data/greetings');
 const Promise = require('bluebird');
 
 const port = 8080;
 
-db.sequelize.sync({ force: true })
-  .then(() => {
-    console.log('Database starting...');
-
-    db.Greet
-      .bulkCreate(data)
-      .then(() => console.log('SUCCESS: Data Inserted'))
-      .then(() => app.listen(port, () => console.log('Server starting...')))
-      .catch(err => console.log(err));
-  })
-  .catch(err => console.log(err));
+app.listen(port, () => console.log('Server starting...'));
 
