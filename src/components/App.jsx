ae514d1100e70eb5940836ac24f47316bc340625
p-herbert <pete.a.herbert@gmail.com>
2017-03-02 00:03:24 -0800

    (refactor) Remove HTTP requests and component state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,19 @@
 import React, { Component } from 'react';
 import * as _ from 'lodash';
-import 'whatwg-fetch';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      greetings: [] };
-
-    this.get = this.get.bind(this);
+    this.state = {};
   }
 
   componentDidMount() {
-    this.get('/greetings', (err, data) => this.setState({ greetings: data }));
-  }
-
-  get(endpoint, cb) {
-    fetch(endpoint)
-      .then(res => res.json())
-      .then(data => cb(null, data))
-      .catch(err => cb(err, null));
   }
 
   render() {
     return (
-      <div>
-        {
-          this.state.greetings
-            .map((lang, index) => <h1 key={index}>{ lang.greeting }</h1>)
-        }
-      </div>
+      <h1>Hello World!</h1>
     );
   }
 }
